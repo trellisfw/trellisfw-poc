8568647995c6f90f9cecf34d707a69e9cc66bfb3
sanoel <noel.samuel.a@gmail.com>
2017-08-31 14:51:42 -0400

    added keys for audit verification to each app

---
+++ pspperfection/pubKey.js
@@ -0,0 +1,4 @@
+let pubKey = { kty: 'RSA',
+  n: 'fLWfnZAr64VJo_Qxns9nT9yQG3UQR4A-YJd2qAmOiAK7DyfRd-gzsaDCuVTk3-57EulOsqFfQ6BaoLW9TdD9WBaPbZL5QQ5v-A_B6AcjSMfGdIkwuSODm6WKN_ijtA1ucbRMP8_PN7MxefRcXW519-x1PHTIbAEdxuOuIsNyUaM',
+  e: 'AQAB' }
+module.exports = pubKey;
---
+++ retailfresh/pubKey.js
@@ -0,0 +1,4 @@
+let pubKey = { kty: 'RSA',
+  n: 'e4FAnVc7c2b_6QpiPrWPr7YEkA6K2ZsllXaq4dTKRj3VpGOisPEsNYV0l2EPDe52C66kGPp6DgVR3Tu1EraXGY6fB7ZMSFhedcFOA-SPr70MTiGliPzhZHBMNYP58AD6dkojXx3DrgYem1uaZUMkQE4mGSYbf0bjcV9CP8Cd9ZU',
+  e: 'AQAB' }
+module.exports = pubKey;
