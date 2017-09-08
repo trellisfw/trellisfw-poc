61092a7577b21b01b355eac46b70348d9771a2a7
Cyrus Bowman <cyrusbow@gmail.com>
2017-09-08 01:40:17 -0400

    Moved TopBar out of App. Not using UserProfile anymore. Added support for local css. CSS files with .module.css ending will be processed as local css.

---
+++ abcaudits/src/common/pure-components/TopBar/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import styles from './index.module.css'
+import { IconButton, MenuItem, Menu } from 'material-ui'
+import Popover from 'material-ui/Popover';
+import PropTypes from 'prop-types';
+
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {popupOpen: false};
+  }
+  closePopup = () => {
+    this.setState({popupOpen: false});
+  }
+  openPopup = () => {
+    this.setState({popupOpen: true});
+  }
+  render() {
+    return (
+      <div className={styles.header} style={this.props.style}>
+        <div className={styles.headerLeft}>
+          <div className={styles.logo}>
+            {this.props.logo}
+          </div>
+          <div className={styles.descContainer}>
+            <div className={styles.title}>
+              <div>{this.props.title || 'Default Title'}</div>
+            </div>
+            <div>
+              {this.props.description || 'A description about the site.'}
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerRight}>
+          <Popover
+              open={this.state.popupOpen}
+              anchorEl={this.refs.profileContainer}
+              anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              onRequestClose={this.closePopup}>
+                  <Menu onTouchTap={this.closePopup}>
+                  		<MenuItem primaryText="Sign out" onTouchTap={()=>{this.props.signOut({})}} />
+                  </Menu>
+          </Popover>
+          {
+						this.props.user ?
+              <div ref='profileContainer' className={styles.profileContainer} onClick={()=>this.openPopup()}>
+                <IconButton iconClassName="material-icons">
+                  account_circle
+                </IconButton>
+                <div>{this.props.user.name}</div>
+              </div>
+						:
+              <div className={styles.profileContainer}>
+  							<div onTouchTap={()=>{this.props.signInClicked({})}}>
+  								Sign in
+  							</div>
+              </div>
+					}
+          {
+            this.props.mode ?
+              <div className={styles.modes}>
+                <p
+                  className={this.props.mode === 'connections' ? styles.highlightedMode : styles.mode }
+                  onClick={()=>{this.props.modeClicked({mode: 'connections'})}}>
+                  <u>{'Connections'}</u>
+                </p>
+                <p
+                  className={this.props.mode === 'certifications' ? styles.highlightedMode : styles.mode}
+                  onClick={()=>{this.props.modeClicked({mode: 'certifications'})}}>
+                  <u>{'Certifications'}</u>
+                </p>
+              </div>
+            :
+            null
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+TopBar.propTypes = {
+  modeClicked: PropTypes.func.isRequired
+};
+
+export default TopBar;
--- abcaudits/src/components/App/index.js
+++ abcaudits/src/components/App/index.js
@@ -1,42 +1,20 @@
 import React from 'react'
 import {connect} from 'cerebral/react'
 import {state, signal} from 'cerebral/tags'
+import styles from './styles.css'
+
 import CertCard from '../CertCard'
 import ClientPanel from '../ClientPanel'
 import SharingDialog from '../SharingDialog'
 import YearPanel from '../YearPanel'
 import UserProfile from '../UserProfile'
 import MainPanel from '../MainPanel'
-import {
-	MenuItem,
-	Divider,
-	IconButton,
-	Checkbox,
-	IconMenu,
-} from 'material-ui'
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
-import styles from './styles.css'
+import TopBar from '../../common/components/TopBar'
+
 
 export default connect({
-  mode: state`app.view.main_panel.mode`,
-  client: state`client_panel.selected_client`,
-  clients: state`client_panel.clients`,
-	certifications: state`app.view.certifications`,
-	sharingDialog: state`sharing_dialog.open`,
 	user: state`user_profile.user`,
-
   initialize: signal`app.initialize`,
-  addCertButtonClicked: signal`app.addCertButtonClicked`,
-	deleteAuditsButtonClicked: signal`app.deleteAuditsButtonClicked`,
-	shareAuditsButtonClicked: signal`sharing_dialog.shareAuditsButtonClicked`,
-	signInClicked: signal`user_profile.signInClicked`,
 },
 
 class App extends React.Component {
@@ -46,47 +24,17 @@ class App extends React.Component {
   }
 
   render() {
-
     let years = {}
-
     return (
       <div className='app'>
-        <div
-          style={{}}
-          className='header'>
-          <div className='header-left'>
-            <h1>ABC Audits</h1>
-            <IconButton
-              iconClassName="material-icons">store
-            </IconButton>
-            Audits done right!
-          </div>
-          <div
-						className='header-right'>
-						{this.props.user ? <UserProfile /> :
-							<p onTouchTap={()=>{this.props.signInClicked({})}}>
-								Sign in
-							</p>
-						}
-            {false ? <div className='modes'>
-              <p
-                className={this.props.mode === 'connections' ? 'highlighted-mode' : 'mode'}
-                onClick={()=>{this.props.modeClicked({mode: 'connections'})}}>
-                <u>Connections</u>
-              </p>
-              <p
-                className={this.props.mode === 'certifications' ? 'highlighted-mode' : 'mode'}
-                onClick={()=>{this.props.modeClicked({mode: 'certifications'})}}>
-                <u>Certifications</u>
-              </p>
-            </div> : null}
-          </div>
-        </div>
+				<TopBar
+          title={'ABC Audits'}
+          description={'Audits done right!'} />
         <div className='lower'>
           <div className='left-panel'>
 						{this.props.user ? <ClientPanel /> : null }
 					</div>
-					{this.props. user ? <MainPanel /> : null}
+					{this.props.user ? <MainPanel /> : null}
         </div>
       </div>
     )
